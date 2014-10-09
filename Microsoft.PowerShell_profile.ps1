a8cec316d71ed78bf1b1000dc505531150609fcd
Shaun Luttin <shaunluttin@gmail.com>
2014-10-09 12:06:48 -0700

    Add ssh-agent to PATH.

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+﻿
+# Add the git SSH agent to the PATH
+# This avoids the "could not find ssh-agent" message
+$env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\bin"
+
+# Load posh-git example profile
+. 'C:\tools\poshgit\dahlbyk-posh-git-95df787\profile.example.ps1'
+
