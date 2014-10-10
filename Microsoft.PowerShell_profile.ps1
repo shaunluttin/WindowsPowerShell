adda7aa2c39c28eb500a7df2937505d07ff556bb
Shaun Luttin <shaun@bigfont.ca>
2014-10-09 17:59:00 -0700

    Use example profile.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,10 +3,10 @@
 $env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\bin"
 
 # Load posh-git example profile
-# . 'C:\tools\poshgit\dahlbyk-posh-git-95df787\profile.example.ps1'
+. 'C:\tools\poshgit\dahlbyk-posh-git-95df787\profile.example.ps1'
 
 # Load my posh-git profile using the dot-source notation
-. 'C:\Users\BigFont\Documents\WindowsPowerShell\My.PoshGitProfile.ps1'
+# . 'C:\Users\BigFont\Documents\WindowsPowerShell\My.PoshGitProfile.ps1'
 
 # Customize the prompt
 function prompt
