e46f1813ff757eddc2fa9a4fd59b7fc4a1c0304e
Shaun Luttin <shaunluttin@gmail.com>
2014-10-10 13:32:10 -0700

    Comment my profile back.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,17 +3,17 @@
 $env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\bin"
 
 # Load posh-git example profile
-. 'C:\tools\poshgit\dahlbyk-posh-git-95df787\profile.example.ps1'
+# . 'C:\tools\poshgit\dahlbyk-posh-git-95df787\profile.example.ps1'
 
 # Load my posh-git profile using the dot-source notation
-# . 'C:\Users\BigFont\Documents\WindowsPowerShell\My.PoshGitProfile.ps1'
+. 'C:\Users\BigFont\Documents\WindowsPowerShell\My.PoshGitProfile.ps1'
 
 # Customize the prompt
-# function prompt
-# {
-#    Write-Host ("Shaun>") -nonewline -foregroundcolor White
-#    return " "
-# }
+function prompt
+{
+   Write-Host ("Shaun>") -nonewline -foregroundcolor White
+   return " "
+}
 
 # Helper functions
 
