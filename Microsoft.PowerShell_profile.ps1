3b8cd15778d3f230a536f0fbcf25fae4d29ea968
Shaun Luttin <shaun@bigfont.ca>
2014-10-09 17:00:52 -0700

    Changes from FONTY.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,14 @@
 ﻿
-# Load posh-git example profile
-. 'C:\tools\poshgit\dahlbyk-posh-git-95df787\profile.example.ps1'
+# Load posh-git example profile using the dot-source notation
+# . 'C:\tools\poshgit\dahlbyk-posh-git-95df787\profile.example.ps1'
 
+# Load my posh-git profile using the dot-source notation
+. 'C:\Users\BigFont\Documents\WindowsPowerShell\My.PoshGitProfile.ps1'
+
+
+
+function prompt
+{
+    Write-Host ("Shaun>") -nonewline -foregroundcolor White
+    return " "
+}
---
+++ My.PoshGitProfile.ps1
@@ -0,0 +1,15 @@
+Push-Location (Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
+
+# Load posh-git module from current directory
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-95df787\posh-git.psm1'
+
+Enable-GitColors
+Pop-Location
+Start-SshAgent -Quiet
+
+$GitPromptSettings.EnableFileStatus = $false # speed up git
+
+git config --global color.status.changed "cyan normal bold"
+git config --global color.status.untracked "cyan normal bold"
+$GitPromptSettings.WorkingForegroundColor    = [ConsoleColor]::Yellow
+$GitPromptSettings.UntrackedForegroundColor  = [ConsoleColor]::Yellow
