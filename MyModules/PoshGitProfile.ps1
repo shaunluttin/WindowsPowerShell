2b94d9c7bd8508caa9b718f080769f10df17c58d
Shaun Luttin <shaunluttin@gmail.com>
2014-12-16 14:23:24 -0800

    Add git to PATH

--- MyModules/PoshGitProfile.ps1
+++ MyModules/PoshGitProfile.ps1
@@ -5,16 +5,17 @@ Import-Module posh-git
 # Use colors
 Enable-GitColors
 
-# Start-SshAgent 
+# Start-SshAgent
 # This commandlet is part of the posh-git module
-# First, add the git SSH agent to the PATH 
+# First, add the git SSH agent to the PATH
 # to avoid "could not find ssh-agent"
 $env:path += ";" + (Get-Item "Env:ProgramFiles(x86)").Value + "\Git\bin"
-Start-SshAgent -Quiet 
+Start-SshAgent -Quiet
 
 # speed up git
-$GitPromptSettings.EnableFileStatus = $false 
+$GitPromptSettings.EnableFileStatus = $false
 
+# set git colours
 # git config --global color.status.changed "cyan normal bold"
 # git config --global color.status.untracked "cyan normal bold"
 # $GitPromptSettings.WorkingForegroundColor    = [ConsoleColor]::Yellow
