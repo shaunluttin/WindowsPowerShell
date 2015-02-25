5e58ee07d2774a1744fad40c5bacfc7c50e5daec
unknown <shaun.luttin@CANL-JBWNK32.gds.ca>
2015-02-25 12:13:53 -0800

    Reorganize into modules to run at work

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,11 @@
 
 Write-Host "Loading MyScripts..."
 
-# Run
 
-Push-Location ~\Documents\WindowsPowerShell\MyScripts
 
-.\AddToPATH.ps1
-.\Aliases.ps1
-.\PoshGitProfile.ps1
-.\RawUiCustomization.ps1
-.\CustomPrompt.ps1
+Import-Module CustomPrompt
+Import-Module Aliases
+Import-Module SkipHistory
 
-Pop-Location
-
-# Load
-
-Import-Module History
 
 Write-Host "Done"
