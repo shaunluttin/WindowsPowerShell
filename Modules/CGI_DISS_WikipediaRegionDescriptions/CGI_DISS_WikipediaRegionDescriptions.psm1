ee5a7a2dd7aba99304e22367d7c70673b36ec335
Shaun Luttin <shaunluttin@gmail.com>
2015-03-09 15:46:27 -0700

    Refactor the Wikipedia module

--- Modules/CGI_DISS_WikipediaRegionDescriptions/CGI_DISS_WikipediaRegionDescriptions.psm1
+++ Modules/CGI_DISS_WikipediaRegionDescriptions/CGI_DISS_WikipediaRegionDescriptions.psm1
@@ -1,39 +1,108 @@
 
 # Example
+# Get-WikipediaRegionDesc -region "tradebc_community.csv"
+
+function Get-WikipediaRegionDescFromCsvFile ($file)
+{
+    $index = 0;
+    $destination = "wikipedia-descriptions.csv";
+
+    try
+    {
+        Import-Csv $file | 
+            # Select-Object -first 3 |
+            Select-Object name | 
+            ForEach-Object { Get-WikipediaRegionDesc $_.name } | 
+            ForEach-Object { 
+                Export-Csv -InputObject $_ -Path $destination -append;
+                Write-Host $_.RegionName "Complete";
+                $index += 1; 
+            }
+
+       Write-Host "Wrote $($index) entries to $($destination)."
+    
+    }
+    catch
+    {
+        Write-Host $_.Exception.ToString()
+    }
+}
+
+
+# Example
 # Get-WikipediaRegionDesc -region "vancouver"
 
 function Get-WikipediaRegionDesc ($region)
 {
-
-  $url = "http://en.wikipedia.org/wiki/$($region),_British_Columbia";
-  $result;
+    $dir = "C:\Users\shaun.luttin\Documents\ProjectNotes\1415-TIBC-DISS-TA-0077\WikipediaRegions\Results\";
   
-  try
-  {
+    $regex = @{};
+    $regex.htmlTags = '<[^>]+>';
+    $regex.footnotes = '\[\d{0,3}\]';
+    $regex.firstNations = '\/[^\/]+\/\s';
+    $regex.parentheses = '\([^\)]+\),?\s';
+    $regex.htmlEntities = '&[^\s]*;';
   
-     $result = curl $url | 
-     select -expand allelements | 
-     ? { $_.id -eq "mw-content-text" } | 
-     select -expand innerHTML | 
-     % { 
-        $i = $_.IndexOf("<P>"); 
-        $j = $_.IndexOf("</P>"); 
-        $_.Substring($i, $j - $i) -replace '<[^>]*>'
-      } 
-  }
-  catch 
-  {
-    $result = "Didn't work. Maybe " + $url + " doesn't exist.";
-  }
+    try
+    {
+        $url = Get-WikipediaUrlFromRegion $region
+        $description = Get-DescriptionFromWikipediaUrl $url
 
-   $dir = "C:\Users\shaun.luttin\documents\CGI_DISS_WikipediaRegionDescriptions\"
-   ni -type dir $dir -force | out-null
-   
-   $file = $dir + $region + ".txt";
-   ni -type file $file -force | out-null
-   ac $file $result;
-   
-   
-   "`n" + $result + "`n`n" + $url + "`n`n";
+        $results = @{
+            RegionName = $region
+            Description = $description
+            ShortDescription = $description.PadRight(380).Substring(0, 380)
+        }
 
+        # convert hash table to object
+        $object = new-object psobject -Property $results
+        return $object;
+
+    }
+    catch 
+    {
+        Write-Host $_.Exception.ToString()
+    }
+
+}
+
+function Get-WikipediaUrlFromRegion ($region)
+{
+    try
+    {
+        $urlSegment = $region -replace " ", "_"
+        $url = "http://en.wikipedia.org/wiki/$($urlSegment),_British_Columbia";
+    }
+    catch
+    {
+        Write-Host $_.Exception.ToString()
+    }
+    
+    return $url;
+}
+
+function Get-DescriptionFromWikipediaUrl ($url)
+{
+    try
+    {
+        $desc = Invoke-WebRequest $url | 
+            Select-Object -expand allelements | 
+            Where-Object { $_.id -eq "mw-content-text" } | 
+            Select-Object -expand innerHTML | 
+            ForEach-Object { 
+                $i = $_.IndexOf("<P>"); 
+                $j = $_.IndexOf("</P>"); 
+                $_.Substring($i, $j - $i) -replace $regex.htmlTags -replace $regex.footnotes -replace $regex.firstNations -replace $regex.parentheses -replace $regex.htmlEntities
+            }    
+    }
+    catch [System.Net.WebException]
+    {
+        Write-Host $url " not found"
+    }
+    catch
+    {
+        Write-Host $_.Exception.ToString()
+    }
+
+    return $desc;
 }
